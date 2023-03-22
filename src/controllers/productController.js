e65ff3a56021da52c5efc2d1f3d38f5f259139e1
Abdellatif-ait <ja_aiteur@esi.dz>
2023-03-22 08:53:22 +0100

    session01: add few comments

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -20,33 +20,46 @@ const data= [
 ]
 
 const getProductPage=(req,res)=>{
+    // parse the object as param to the ejs file with the name product and send it fully rendred for the client 
     res.render('product',{products:data})
 }
 
 const getEditPage=(req,res)=>{
+    // get the id from the req (since it's a param, we could access is using req.params)
     const {id}= req.params
+    // parse the object data[id] as param to the ejs file with the name editProduct and send it fully rendred for the client
     res.render('editProduct',{product:data[id]})
 }
 const createHandler=(req,res)=>{
+    // get the data sent by the user in the req.body
     const {name,description,price}=req.body
+    // add the product sent by the user to our data variable
     data.push({
         id:data.length,
         name,
         description,
         price
     })
+    // redirect the client to the /product route
     res.redirect('/product')
 
 }
 const editHandler=(req,res)=>{
+    // get the id from the req 
     const {id}= req.params
+    // get the data sent by the user in the req.body
     const {name,description,price}=req.body
+    // update the data[id] element with the new data
     data[id]={id,name,description,price}
+    // redirect the client to the /product route
     res.redirect('/product')
 }
 const deletehandler=(req,res)=>{
+    // get the id from the req (since it's a param, we could access is using req.params)
     const {id}= req.params
+    // delete the element with id index in the array 
     data.splice(id,1)
+    // redirect the client to the /product route
     res.redirect('/product')
 }
 
